842ec287ac744b8df5ac73e4f2fe17502a9b6799
jaison080 <jaisondennis080@gmail.com>
2022-03-25 18:22:06 +0530

    Inital commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
-
 ReactDOM.render(
   <ChakraProvider>
     <BrowserRouter>
